143bec01f787f0f528fa745cc613b49343b9c68a
tania <tanislavens@yahoo.com>
2019-06-19 17:32:40 -0400

    added collapse buttons

--- Article/Article.js
+++ Article/Article.js
@@ -4,12 +4,15 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+    this.buttonPanel = domElement.querySelector('.buttonPanel');
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.expandButton.textContent = "Expand";
+    //Added a collapse button to HTML
+    this.collapseButton = domElement.querySelector('.collapseButton');
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () =>
+    this.buttonPanel.addEventListener('click', () =>
       this.expandArticle());
   }
   expandArticle() {
